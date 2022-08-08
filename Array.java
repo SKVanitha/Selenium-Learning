36f02aac4cd470c8b568e6c2bac99a81f0cddc53
SKVanitha <110803819+SKVanitha@users.noreply.github.com>
2022-08-08 14:25:36 +0530

    Initial commit

---
+++ Array.java
@@ -0,0 +1,37 @@
+package org.array;
+
+    //public class Array {
+	
+	
+	//public static void main(String args[]) {
+		//int b=0;
+		//int a[]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		//for(int i=0;i<a.length;i++) {
+			
+			//b=b+a[i];
+		//}
+			//System.out.println(b);
+		
+	
+//	}
+
+//}
+public class Array {
+	
+	
+	public static void main(String args[]) {
+		int b=0;  double c=0.0;
+		int a[]= {1,2,3,4,5,6,7,8,9,10};
+		for(int i=0;i<a.length;i++) {
+			
+			b=b+a[i];
+			
+		}
+		
+		    c=b/a.length;
+			System.out.println(" Average is:"+c);
+		
+	
+	}
+
+}
